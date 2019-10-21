0c9547f190cf876b4f4103ebc03d86af320fca40
alexanderspace100 <alexanderspace100@gmail.com>
2019-10-21 22:31:38 +0200

    Testing connectivity.

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -7,13 +7,14 @@ describe 'List books' do
 
     repository.create(title: 'PoEAA', author: 'Martin Fowler')
     repository.create(title: 'TDD',   author: 'Kent Beck')
+    repository.create(title: 'DDD',   author: 'Kent Beck')
   end
 
   it 'displays each book on the page' do
     visit '/books'
 
     within '#books' do
-      assert page.has_css?('.book', count: 2), 'Expected to find 2 books'
+      assert page.has_css?('.book', count: 3), 'Expected to find 3 books'
     end
   end
 end
